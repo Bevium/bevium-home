24979c6dd50d431dd0bb5173da92f410683cb343
Daniele Calanna <dev.daniele.calanna@gmail.com>
2023-06-20 12:46:40 +0200

    Make the website ready for the first release

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,7 @@
   let heroSection = select('#hero')
   if (heroSection) {
     const heroScrolled = ()  => {
-      var scaleSize = ((1 + (0.0005 * window.scrollY)) > 1.3) ? 1.3 : 1 + (0.0005 * window.scrollY);
+      var scaleSize = ((1 + (0.0005 * window.scrollY)) > 1.2) ? 1.2 : 1 + (0.0005 * window.scrollY);
       console.log(scaleSize)
 
       heroSection.style.transform =  `scale(${scaleSize})`;
