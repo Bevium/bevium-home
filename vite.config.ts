333b86de02c4b0dbbaf51f80b41e76a1cdef1a4f
federico-arona <federico.arona.94@gmail.com>
2025-09-01 18:25:31 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '"https://bevium.github.io/bevium-home',
+  base: '/bevium-home/',
 }));
 
